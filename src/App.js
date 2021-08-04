04ba7a2cebe44024f9af6cb4ee652758cd01b1a7
Brendan Edwards <brendanae16@gmail.com>
2021-08-03 23:16:04 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Tweet from './Tweet';
+
+function App() {
+
+  const [users, setUsers] = useState([
+    { name: 'Ed', message: 'Hello there' },
+    { name: 'John', message: 'I am John Snow' },
+    { name: 'traversy', message: 'I am awesome' }
+  ])
+
+  return (
+    <div className='app'>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
+
+
